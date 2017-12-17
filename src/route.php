5c2f7f8f6211b61f1c61f21fcaef7bea7018ac41
Yulia <orig.www@gmail.com>
2017-12-17 16:54:50 +0200

    framework

---
+++ src/route.php
@@ -0,0 +1,12 @@
+<?php
+
+use Symfony\Component\Routing;
+
+$routes = new Routing\RouteCollection();
+
+$routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', array(
+    'year' => null,
+    '_controller' => 'Calendar\Controller\LeapYearController::indexAction',
+)));
+
+return $routes;
---
+++ web/index.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel;
+use Symfony\Component\Routing;
+
+$request = Request::createFromGlobals();
+
+$routes = include __DIR__.'/../src/route.php';
+
+$context = new Routing\RequestContext();
+$matcher = new Routing\Matcher\UrlMatcher($routes, $context);
+
+$controllerResolver = new HttpKernel\Controller\ControllerResolver();
+$argumentResolver = new HttpKernel\Controller\ArgumentResolver();
+
+$framework = new \Simplex\Framework($matcher, $controllerResolver, $argumentResolver);
+$response = $framework->handle($request);
+
+$response->send();
